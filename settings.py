f8f5a741faf5bf9a3d44959664e1e449a2f00968
gurkc006 <gurkc006@users.noreply.github.com>
2024-02-06 17:27:37 +0100

    Update settings.py
    Changed to my hardware settings (LiFePo4)

--- settings.py
+++ settings.py
@@ -4,9 +4,9 @@
 ########## Hardware settings ##########
 #######################################
 
-NR_OF_BATTERIES = 2                                     # Nr. of physical batteries to be aggregated. Smart shunt for battery current is not needed and not supported. 
-NR_OF_CELLS_PER_BATTERY = 22
-NR_OF_MPPTS = 1                                         # Nr. of MPPTs
+NR_OF_BATTERIES = 1                                     # Nr. of physical batteries to be aggregated. Smart shunt for battery current is not needed and not supported. 
+NR_OF_CELLS_PER_BATTERY = 16
+NR_OF_MPPTS = 4                                         # Nr. of MPPTs
 DC_LOADS = False                                        # If DC loads with Smart Shunt present, can be used for total current measurement
 INVERT_SMARTSHUNT = False                               # False: Current subtracted, True: Current added 
 
@@ -32,7 +32,7 @@ READ_TRIALS = 10                                        # Trials to get consiste
 CURRENT_FROM_VICTRON = True                             # If True, the current measurement by Multis/Quattros and MPPTs is taken instead of BMS.
                                                         # Necessary for JK BMS due to poor precision.
 OWN_SOC = True                                          # If True, the self calculated charge indicators are taken instead of BMS
-ZERO_SOC = True                                         # Allow zeroing charge counter at MIN_CELL_VOLTAGE. At full battery it is always set to 100%. 
+ZERO_SOC = False                                        # Allow zeroing charge counter at MIN_CELL_VOLTAGE. At full battery it is always set to 100%. 
 CHARGE_SAVE_PRECISION = 0.0025                          # Trade-off between save precision and file access frequency
 
 #######################################
@@ -43,27 +43,27 @@ CHARGE_SAVE_PRECISION = 0.0025                          # Trade-off between save
 
 OWN_CHARGE_PARAMETERS = True                            # Calculate own charge/discharge control parameters (True) from following settings
                                                         # or use them from battery driver (False)
-BALANCING_VOLTAGE = 2.45                                # This voltage per cell will be set periodically and kept until balancing below CELL_DIFF_MAX and next charge cycle
-BALANCING_REPETITION = 10                               # in days                      
+BALANCING_VOLTAGE = 3.45                                # This voltage per cell will be set periodically and kept until balancing below CELL_DIFF_MAX and next charge cycle
+BALANCING_REPETITION = 1                                # in days                      
 
-CHARGE_VOLTAGE_LIST = [2.45, 2.45, 2.42, 2.40, 2.40, 2.35, 2.35, 2.35, 2.40, 2.42, 2.45, 2.45]      # Set up how full the battery has to be charged in given month
+CHARGE_VOLTAGE_LIST = [3.45, 3.45, 3.45, 3.45, 3.45, 3.45, 3.45, 3.45, 3.45, 3.45, 3.45, 3.45]      # Set up how full the battery has to be charged in given month
 #                       jan,  feb,  mar,  apr,  may,  jun,  jul,  aug,  sep,  oct,  nov,  dec
 
-MAX_CELL_VOLTAGE = 2.5                                  # If reached by 1-st cell, the CVL is dynamically limited. DC-coupled PV feed-in will be disabled to enable the CCL. 
-MIN_CELL_VOLTAGE = 1.9                                  # If reached, discharge current set to zero
+MAX_CELL_VOLTAGE = 3.5                                  # If reached by 1-st cell, the CVL is dynamically limited. DC-coupled PV feed-in will be disabled to enable the CCL. 
+MIN_CELL_VOLTAGE = 2.9                                  # If reached, discharge current set to zero
 MIN_CELL_HYSTERESIS = 0.1                               # Allow discharge above MIN_CELL_VOLTAGE + MIN_CELL_HYSTERESIS
 CELL_DIFF_MAX = 0.015                                   # If lower: re-enable DC-coupled PV feed in (if was disabled by dynamic CVL reduction and was enabled before); go back from BALANCING_VOLTAGE to CHARGE_VOLTAGE
 BATTERY_EFFICIENCY = 0.98                               # Ah fed into batteries are multiplied by efficiency 
 
-MAX_CHARGE_CURRENT = 300                                # Max. charge current at normal conditions
-MAX_DISCHARGE_CURRENT = 200                             # Max. discharge current at normal conditions
+MAX_CHARGE_CURRENT = 150                                # Max. charge current at normal conditions
+MAX_DISCHARGE_CURRENT = 100                             # Max. discharge current at normal conditions
 
 # settings limiting charge and discharge current if at least one cell gets full or empty
 # the lists may have any length, but the same length for voltage and current
 # linear interpolation is used for values between
 
 CELL_CHARGE_LIMITING_VOLTAGE = [MIN_CELL_VOLTAGE, MIN_CELL_VOLTAGE + 0.05, BALANCING_VOLTAGE - 0.1, BALANCING_VOLTAGE, MAX_CELL_VOLTAGE] # [min, ... ,max]; low voltage: limiting current from grid
-CELL_CHARGE_LIMITED_CURRENT =  [0.2, 1, 1, 0.05, 0]
+CELL_CHARGE_LIMITED_CURRENT =  [0.1, 1, 1, 0.05, 0]
 CELL_DISCHARGE_LIMITING_VOLTAGE = [MIN_CELL_VOLTAGE, MIN_CELL_VOLTAGE + 0.1, MIN_CELL_VOLTAGE + 0.2]    # [min, ... ,max]
 CELL_DISCHARGE_LIMITED_CURRENT =  [0, 0.05, 1]
 
