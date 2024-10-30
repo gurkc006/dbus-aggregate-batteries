c7e7e1702ffeb494425cc0d84f3814ede4ec344b
gurkc006 <gurkc006@users.noreply.github.com>
2024-10-30 08:03:17 +0100

    save

--- aggregatebatteries.py
+++ aggregatebatteries.py
@@ -871,9 +871,9 @@ class DbusAggBatService(object):
             elif (self._EssActive == 3):
                 AcPowerSetpoint = min(ASP1,ASP2)
             elif (self._EssActive == 4):
-                AcPowerSetpoint = min(ASP1,ASP4)
-                if (Soc < MinimumSocLimit):
-                    ACPowerSetpoint = ASP_noDischarge
+                #AcPowerSetpoint = min(ASP1,ASP4)
+                #if (Soc < MinimumSocLimit):
+                ACPowerSetpoint = ASP_noDischarge
             elif (self._EssActive == 5):
                 AcPowerSetpoint = min(ASP5,ASP4)
                 if (Soc < MinimumSocLimit):
