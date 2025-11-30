5760bb6760bc5c8ce71f68dd853bef54d5624c6f
gurkc006 <gurkc006@users.noreply.github.com>
2025-11-30 11:53:31 +0100

    fix permissions

--- update.sh
+++ update.sh
@@ -5,3 +5,11 @@ unzip -o aggregatebatteriesEss.zip
 rm aggregatebatteriesEss.zip
 /bin/cp -rf dbus-aggregate-batteries-main/* .
 /bin/rm -R dbus-aggregate-batteries-main
+
+# fix permissions, owner and group
+if [ -d "/data/apps/dbus-aggregate-batteries" ]; then
+    chmod +x /data/apps/dbus-aggregate-batteries/*.sh
+    chmod +x /data/apps/dbus-aggregate-batteries/*.py
+    chmod +x /data/apps/dbus-aggregate-batteries/service/run
+    chmod +x /data/apps/dbus-aggregate-batteries/service/log/run
+fi
