aaacc084f3a37a456781dc2a85a1d7ff471c64c6
gurkc006 <gurkc006@users.noreply.github.com>
2025-11-30 15:32:05 +0100

    save

--- dbus-aggregate-batteries.py
+++ dbus-aggregate-batteries.py
@@ -798,10 +798,11 @@ class DbusAggBatService(object):
         try:
             #logging.info("GRID_SERVICE_NAME=%s" % settings.GRID_SERVICE_NAME)
             for service in self._dbusConn.list_names():
-                logging.info("> service=%s" % (service))
+                logging.info("> service = %s" % (service))
                 if settings.GRID_SERVICE_NAME in service:
                 #    self._grid = service
-                #    logging.info("|- %s found." % ((self._dbusMon.dbusmon.get_value(service, "/ProductName")),))
+                    logging.info("|- %s found." % ((self._dbusMon.dbusmon.get_value(service, "/ProductName")),))
+                    pass
         except Exception:
             (
                 exception_type,
