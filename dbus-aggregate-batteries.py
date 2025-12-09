456fb8cbe0338e09809a10da4e6ccf140fe004fb
gurkc006 <gurkc006@users.noreply.github.com>
2025-12-09 11:28:34 +0100

    save

--- dbus-aggregate-batteries.py
+++ dbus-aggregate-batteries.py
@@ -1543,11 +1543,11 @@ class DbusAggBatService(object):
             elif (self._EssActive == 4):  # winter?!
                 AcPowerSetpoint = min(APSp1,APSp4)
                 if (Soc < MinimumSocLimit):
-                    AcPowerSetpoint = max(AcPowerSetpoint,APSp_noDischarge)
+                    AcPowerSetpoint = max(min(APSp1,APSp4),APSp_noDischarge)
             elif (self._EssActive == 5): # summer
                 AcPowerSetpoint = min(APSp5,APSp4)
                 if (Soc < MinimumSocLimit):
-                    AcPowerSetpoint = max(AcPowerSetpoint,APSp_noDischarge)
+                    AcPowerSetpoint = max(min(APSp5,APSp4),APSp_noDischarge)
             self._dbusMon.dbusmon.set_value(self._multi, '/Hub4/L1/AcPowerSetpoint',AcPowerSetpoint)
         else:
             AcPowerSetpoint = self._dbusMon.dbusmon.get_value(self._multi, '/Hub4/L1/AcPowerSetpoint')
